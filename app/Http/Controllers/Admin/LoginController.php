9ced8b8867f11fb87ce60d839cc7c359c937ccdf
Simon <6692223@qq.com>
2018-11-06 00:57:40 +0800

    login.code commit

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,13 +5,25 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+
 require_once 'resources/org/code/Code.class.php';
 
 class LoginController extends CommonContriller
 {
     public function login()
     {
-        return view('admin.login');
+        if($input = Input::all()){
+            $code = new \Code;
+            $getcode = $code->get();
+            if(strtoupper($input['code'])!=$getcode){
+                return back()->with('msg','验证码错误');
+            }else{
+                echo 'OK';
+            }
+        }else{
+            return view('admin.login');
+        }
     }
 
     public function code()
@@ -20,9 +32,4 @@ class LoginController extends CommonContriller
         $code->make();
     }
 
-    public function getCode()
-    {
-        $code = new \Code;
-        echo $code->get();
-    }
 }
